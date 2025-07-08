fe33a792ede929742ae6553d502ba3b29ded6821
venky2809 <jvenkatesh494@gmail.com>
2025-07-08 21:11:16 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -118,7 +118,7 @@ resource "aws_security_group" "demo_sg" {
 
 # EC2 Instance
 resource "aws_instance" "demo_instance" {
-  ami                    = "ami-03f65b8614a860c29" // Replace with a valid AMI ID for your region
+  ami                    = "ami-044415bb13eee2391" // Replace with a valid AMI ID for your region
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.demo_subnet.id
   key_name               = aws_key_pair.demo_keypair.key_name
